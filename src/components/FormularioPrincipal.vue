460062328d1110bffc1c8f43a112f36a97ab194a
Ariane de Almeida Franchetto <nanifranchetto@gmail.com>
2022-11-08 22:29:50 -0300

    Inicio do código
    Botões de play e pause com funções

---
+++ src/components/FormularioPrincipal.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="box">
+        <div class="column">
+            <div class="column is-8" role="form" aria-label="Formulário para a criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
+            </div>
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+                    <section>
+                        <strong>{{ tempoEmSegundos }}</strong> <!-- imprimindo a função tempo em segundos -->
+                    </section>
+                    <button class="button" @click="iniciar">  <!-- @click é usado para adicionar o método (comportamento) -->
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button" @click="finalizar">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: "FormularioPrincipal",
+
+    data() {
+        return { /*ESTADO, RETORNA UM OBJETO ONDE TEM AS INFORMAÇÕES PERTINENTES*/
+tempoEmSegundos: 0,
+cronometro: 0
+        }
+    },
+
+    computed: { /*MONITORA INFORMAÇÕES, CONFORME É ALTERADA ELE REAGE E ATUALIZA */
+tempoDecorrido() : string {
+    return new Date(this.tempoEmSegundos * 1000).toISOString().substring(8,11)
+}
+    },
+
+    methods: { /*COMPORTAMENTOS */
+        iniciar() {
+            /*começar a contagem, setInterval
+            1 seg = 1000 ms
+            
+            */
+       this.cronometro = setInterval(() => {
+                this.tempoEmSegundos +=1
+
+            }, 1000)
+
+        },
+
+        finalizar() {
+            clearInterval(this.cronometro)
+        }
+
+       
+    }
+
+
+})
+
+</script>
+
+<script scoped>
+
+
+
+
+</script>
